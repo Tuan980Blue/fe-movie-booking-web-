6907579f41594e50b429eb65dc3369fb25461a22
Tuan980Blue <tuanmeo980provip@gmail.com>
2025-09-25 09:54:10 +0700

    Fix folder

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
-import AuthPage from "@/features/auth/AuthPage";
+import AuthPage from "../features/auth/AuthPage";
 
 // Simple router with only Home and Auth
 
